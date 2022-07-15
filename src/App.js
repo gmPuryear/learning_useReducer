ed0e2b611b30ae0872e685482bc52b8c32694a68
George Puryear <gm.puryear@yahoo.com>
2022-07-15 16:43:27 -0500

    created simple react counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        // this VV arrow function automatically returns prevCount => prevCount + 1
+        setCount(prevCount => prevCount + 1);
+    }
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1);
+    }
+
+    return (
+
+        <div className="flex flex-col justify-center h-screen ">
+            <div className="flex justify-center">
+                    <span className="flex justify-center border-2 border-solid border-blue-700 text-red-700">
+                        {count}
+                    </span>
+            </div>
+            <button onClick={decrement}>
+                UP
+            </button>
+        </div>
+
+
+    );
 }
 
 export default App;
